915a14e4f7a0830ad5441f3d161743588faab240
George Z. Zachos <gzachos@cse.uoi.gr>
2020-05-04 00:51:29 +0300

    Added initial bare implementation of search

--- LuceneTest/src/com/gzachos/ir/MainTest.java
+++ LuceneTest/src/com/gzachos/ir/MainTest.java
@@ -1,13 +1,15 @@
 package com.gzachos.ir;
 
 import com.gzachos.ir.lucene.IndexFiles;
+import com.gzachos.ir.lucene.SearchFiles;
 
 public class MainTest {
 
 	public static void main(String[] args) {
 		boolean overwriteIndex = false;
-		
 		IndexFiles.createIndex(overwriteIndex);
+		SearchFiles.searchFiles();
+		System.out.println("Terminated...");
 	}
 
 }
---
+++ LuceneTest/src/com/gzachos/ir/lucene/SearchFiles.java
@@ -0,0 +1,78 @@
+package com.gzachos.ir.lucene;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+import com.gzachos.ir.Config;
+
+public class SearchFiles {
+	
+	public static void searchFiles() {
+		try {
+			String field = "title";
+			Path indexDirPath = Paths.get(Config.INDEX_PATH);
+			IndexReader indexReader = DirectoryReader.open(FSDirectory.open(indexDirPath));
+			IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+			Analyzer standardAnalyzer = new StandardAnalyzer();
+			
+			InputStreamReader inStreamReader =  new InputStreamReader(System.in, StandardCharsets.UTF_8);
+			BufferedReader inputReader = new BufferedReader(inStreamReader);
+			QueryParser queryParser = new QueryParser(field, standardAnalyzer);
+			while (true) {
+				System.out.print("> ");
+				System.out.flush();
+				String line = inputReader.readLine();
+				if (line == null || line.length() == -1)
+					break;
+				line = line.trim();
+				if (line.length() == 0)
+					break;
+				Query query = queryParser.parse(line);
+				System.out.println("Searching for: " + query.toString(field) + " in field " + field);
+				doPagingSearch(indexSearcher, query, 10);
+			}
+			
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void doPagingSearch(IndexSearcher indexSearcher, Query query, int hitsPerPage) throws IOException {
+		TopDocs searchResults = indexSearcher.search(query, hitsPerPage);
+		ScoreDoc[] hits = searchResults.scoreDocs;
+		int numTotalHits = Math.toIntExact(searchResults.totalHits.value);
+		System.out.println(numTotalHits + " total matching documents");
+		
+		for (int i = 0; i < hits.length; i++) {
+			Document doc = indexSearcher.doc(hits[i].doc);
+			String url = doc.get("url");
+			String title = doc.get("title");
+			String summary = doc.get("summary");
+			if (url != null) {
+				System.out.println(i + " - " + title + " - " + url);
+				System.out.println(summary + "\n");
+			}
+		}
+	}
+	
+}
