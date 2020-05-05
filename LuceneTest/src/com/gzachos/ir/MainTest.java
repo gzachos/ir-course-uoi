de69927a4013800d1fe96b25de0437ad91ee102c
George Z. Zachos <gzachos@cse.uoi.gr>
2020-05-06 02:16:49 +0300

    Added user prompt as of index overwriting

--- LuceneTest/src/com/gzachos/ir/MainTest.java
+++ LuceneTest/src/com/gzachos/ir/MainTest.java
@@ -1,5 +1,7 @@
 package com.gzachos.ir;
 
+import java.util.Scanner;
+
 import com.gzachos.ir.lucene.IndexFiles;
 import com.gzachos.ir.lucene.SearchFiles;
 
@@ -7,6 +9,16 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		boolean overwriteIndex = false;
+		
+		if (overwriteIndex) {
+			@SuppressWarnings("resource")
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("About to overwrite existing index. Are you sure (y/N)? ");
+			String userInput = scanner.nextLine();
+			if (!(userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")))
+				overwriteIndex = false;
+			scanner = null; // Don't close scanner as System.in will be closes too.
+		}
 		IndexFiles.createIndex(overwriteIndex);
 		SearchFiles.searchFiles();
 		System.out.println("\nTerminated...");
