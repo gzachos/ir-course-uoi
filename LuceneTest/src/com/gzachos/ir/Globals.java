f742e8bfe6528770fb14dee1aa85030ea0bd9abf
George Z. Zachos <gzachos@cse.uoi.gr>
2020-05-09 23:06:02 +0300

    Updated paging search to exclude same-title articles

--- LuceneTest/src/com/gzachos/ir/Globals.java
+++ LuceneTest/src/com/gzachos/ir/Globals.java
@@ -2,4 +2,5 @@ package com.gzachos.ir;
 
 public class Globals {
 	public static final String[] DOCUMENT_FIELDS = {"title", "content", "multimedia", "quotes"};
+	public static final int HITS_PER_PAGE = 10;
 }
--- LuceneTest/src/com/gzachos/ir/lucene/SearchFiles.java
+++ LuceneTest/src/com/gzachos/ir/lucene/SearchFiles.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -19,6 +21,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TotalHits;
 import org.apache.lucene.store.FSDirectory;
 
 import com.gzachos.ir.Config;
@@ -49,7 +52,7 @@ public class SearchFiles {
 					break;
 				Query query = queryParser.parse(line);
 				System.out.println("Searching for: \"" + query.toString() + "\"");
-				doPagingSearch(indexSearcher, query, 10);
+				doPagingSearch(indexSearcher, query, 5);
 			}
 			
 		} catch (IOException | ParseException e) {
@@ -60,22 +63,80 @@ public class SearchFiles {
 		}
 	}
 	
-	public static void doPagingSearch(IndexSearcher indexSearcher, Query query, int hitsPerPage) throws IOException {
-		TopDocs searchResults = indexSearcher.search(query, hitsPerPage);
-		ScoreDoc[] hits = searchResults.scoreDocs;
-		int numTotalHits = Math.toIntExact(searchResults.totalHits.value);
-		System.out.println(numTotalHits + " total matching documents\n");
-		
-		for (int i = 0; i < hits.length; i++) {
-			Document doc = indexSearcher.doc(hits[i].doc);
-			String url = doc.get("url");
-			String title = doc.get("title");
-			String summary = doc.get("summary");
-			if (url != null) {
-				System.out.println((i+1) + " - " + title + " - " + url);
-				System.out.println(summary);
+	public static void doPagingSearch(IndexSearcher indexSearcher, Query query, int numPages) throws IOException {
+		int docsToFetch = numPages * Globals.HITS_PER_PAGE;
+		long startTime = System.currentTimeMillis();
+		TopDocs searchResults = indexSearcher.search(query, docsToFetch + Globals.HITS_PER_PAGE);
+		double searchTime = (System.currentTimeMillis() - startTime) / 1000.0;
+		ScoreDoc[] hitsArray = searchResults.scoreDocs;
+		ArrayList<ScoreDoc> hits = new ArrayList<ScoreDoc>(Arrays.asList(hitsArray));
+		ArrayList<Document> returnedDocs = new ArrayList<Document>();
+		TotalHits totalHits = searchResults.totalHits; 
+		int numTotalHits = Math.toIntExact(totalHits.value);
+		if (numTotalHits == 0) {
+			System.out.println("Found 0 results");
+			return;
+		}
+		String relationStr = (totalHits.relation == TotalHits.Relation.GREATER_THAN_OR_EQUAL_TO) ? "More than" : "About";
+		System.out.println("\n" + relationStr + " " + numTotalHits + " results (" + searchTime + " seconds)\n");
+		ScoreDoc lastFetchedDoc = hits.get(hits.size()-1);
+
+		int end = docsToFetch;
+		boolean notEnoughTotalHits = false;
+		if (docsToFetch >= numTotalHits) {
+			end = numTotalHits;
+			notEnoughTotalHits = true;
+		}
+		int skippedDocs = 0, page = 0;
+		int numReturnedDocs = 0;
+
+		for (int i = 0; i < end; i++) {
+			boolean skip = false;
+			Document doc = indexSearcher.doc(hits.get(i).doc);
+			for (int j = 0; j < returnedDocs.size(); j++) {
+				Document prevDoc = returnedDocs.get(j);
+				if (prevDoc.get("title").equals(doc.get("title"))) {
+					skip = true;
+					skippedDocs++;
+					break;
+				}
+			}
+			
+			if (!skip) {
+				returnedDocs.add(doc);
+				numReturnedDocs = returnedDocs.size();
+				
+				String url = doc.get("url");
+				String title = doc.get("title");
+				String summary = doc.get("summary");
+				
+				if (url != null) {
+					System.out.println(numReturnedDocs + " - '" + title + "' - " + url);
+				//	System.out.println(summary);
+				}
+				
+				if (numReturnedDocs == numTotalHits || numReturnedDocs == docsToFetch)
+					break;
+				
+				if (numReturnedDocs % Globals.HITS_PER_PAGE == 0)
+					System.out.println("\n################## Page " + (++page + 1) + "\n");
+			}
+
+			if (i == (end-1) && !notEnoughTotalHits) {
+				int newDocsToFetch = Math.max(Globals.HITS_PER_PAGE, docsToFetch - numReturnedDocs);
+				// System.out.println("About to fetch: " + newDocsToFetch + " more docs");
+				TopDocs newSearchResults = indexSearcher.searchAfter(lastFetchedDoc, query, newDocsToFetch);
+				ScoreDoc[] newHitsArray = newSearchResults.scoreDocs;
+				ArrayList<ScoreDoc> newHits = new ArrayList<ScoreDoc>(Arrays.asList(newHitsArray));
+				hits.addAll(newHits);
+				int numNewHits = newHits.size();
+				// System.out.println("Fetched " + numNewHits + " more docs");
+				end += numNewHits;
+				lastFetchedDoc = hits.get(hits.size()-1);
 			}
 		}
+		System.out.println("\n\nreturned: " + returnedDocs.size());
+		System.out.println("skipped:  " + skippedDocs);
 	}
 	
 }
